5cd0fd47e9de3e3fbcf7f6b7b5acb305f5a13cd6
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-14 20:11:47 +0100

    По новому восстановил 07

---
+++ src/ForecastDay.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DateFormatForecast from "./DateFormatForecast";
+
+export default function ForecastDay(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.city]);
+
+  function ForecastrResponse(respons) {
+    setForecast(respons.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <ul className="DayForecast">
+        {forecast.map(function (dailyforecast, index) {
+          if (index < 5) {
+            return (
+              <li key={index}>
+                <DateFormatForecast dateForecast={dailyforecast} />
+              </li>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </ul>
+    );
+  } else {
+    const APIkey = "bd79ao40tde3dec118ca46bc3e6dd55f";
+    let ApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${APIkey}&units=metric`;
+    axios.get(ApiUrl).then(ForecastrResponse);
+    return null;
+  }
+}
